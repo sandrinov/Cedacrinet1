4dbe2c9572e9dc785bf1ed19494118ac0572f162
Sandro Vecchiarelli <sandrov@live.it>
2017-09-28 10:32:16 +0200

    Added Web Service

---
+++ BankServices/BankClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BankServices
+{
+    public class BankClient
+    {
+        public int ClientID { get; set; }
+        public String ClientName { get; set; }
+    }
+}
---
+++ BankServices/HRService.asmx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace BankServices
+{
+    /// <summary>
+    /// Summary description for HRService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class HRService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public List<BankClient> GetAllClients()
+        {
+            List<BankClient> resultList = new List<BankClient>() {
+                new BankClient(){ ClientID=1, ClientName="Norma,W. Castillo" },
+                new BankClient(){ ClientID=2, ClientName="Amanda,R. Anderson" },
+                new BankClient(){ ClientID=3, ClientName="Patrick,L. Eley" },
+                new BankClient(){ ClientID=4, ClientName="Gwendolyn,S. Brooks" },
+                new BankClient(){ ClientID=5, ClientName="Brenda,L. Owens" },
+                new BankClient(){ ClientID=6, ClientName="Joyce,S. Scheibero" },
+                new BankClient(){ ClientID=7, ClientName="Chris,M. Stinson" }
+            };
+            return resultList;
+        }
+    }
+}
